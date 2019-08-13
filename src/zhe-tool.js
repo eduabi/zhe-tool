b34845671e9e7bd885bac21f95aa1274de35efc6
eduabi <eduabi@163.com>
2019-08-13 22:41:49 +0800

    新增数组去重

--- src/zhe-tool.js
+++ src/zhe-tool.js
@@ -208,6 +208,27 @@ module.exports = {
                 return quickSort(left).concat([basicItem], quickSort(right));
             }
         }
+    },
+    /**
+     * @description 数组操作算法集合
+     */
+    array: {
+        /**
+         * @description 数组去重
+         * @param {Array} data 
+         * @template [15,55,24,88,17]
+         */
+        removeDistinct: function (data) {
+            let obj = {};
+            let resArray = [];
+            for (let item of data) {
+                if (!obj[item]) {
+                    resArray.push(item);
+                    obj[item] = item;
+                }
+            }
+            return resArray;
+        }
     }
 }
 
